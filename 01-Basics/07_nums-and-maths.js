5f23809408843ea6f7fe4bc2c45e17675f7891e9
Siraj Khan <khansiraj99786@gmail.com>
2026-07-07 20:36:32 +0530

    added more

---
+++ 01-Basics/07_nums-and-maths.js
@@ -0,0 +1,32 @@
+const score = 400;
+// console.log(score);
+
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.89666;
+
+// console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.66));
+// console.log(Math.min(2,4,6,78,0))
+// console.log(Math.max(2,4,6,78,0))
+
+console.log((Math.random()*10) + 1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
