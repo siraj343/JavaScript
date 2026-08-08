07bce67b649eb77412df131c60a1f871a414bd3d
Siraj Khan <khansiraj99786@gmail.com>
2026-08-08 18:47:41 +0530

    idk

---
+++ 06-DOM/dom-4/script.js
@@ -0,0 +1,12 @@
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+// let name = div.getAttribute("name");
+// console.log(name);
+
+let div = document.querySelector("div");
+
+div.style.background = "green";
+div.style.fontSize = "25px";
