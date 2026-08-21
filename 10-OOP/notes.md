d30dd6ad27b638b9ca5ac8c905f2abe92f0d107b
Siraj Khan <khansiraj99786@gmail.com>
2026-08-21 23:59:54 +0530

    just started with oop

---
+++ 10-OOP/notes.md
@@ -0,0 +1,22 @@
+# javascript and classes
+Java script is prototype based language
+
+## OOP
+
+## Object
+- collection of properties and methods
+
+## why use OOP
+-Object literal
+
+-constructor function
+-protypes
+-classes
+instances(new, this)
+
+## 4 pillars
+
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism 
---
+++ 10-OOP/oop.js
@@ -0,0 +1,15 @@
+const user = {
+    username: "Siraj",
+    age: 20,
+    class: "TE",
+
+    getUserDetails: function(){
+        console.log("Got user details from data base");
+        console.log(`Username: ${this.username}`);
+        console.log(this)
+        
+    }
+}
+
+console.log(user.age);
+console.log(user.getUserDetails());
